f3090cd4ef7cff0001838a550bb087cca6ec74ae
ag105020 <31078037+ag105020@users.noreply.github.com>
2020-12-15 10:27:55 -0500

    Add files via upload

--- p001_01_Specific_value_low_pH.py
+++ p001_01_Specific_value_low_pH.py
@@ -42,35 +42,51 @@ def fig(pH,CO2,HCO3,CO3):
     legend(loc=1,edgecolor='k')
 
 #PM and IE
+pH,CO2_PM,HCO3_PM,CO3_PM = Cchem(14,2191,34,1025.442,8.102)
 pH,CO2_IE,HCO3_IE,CO3_IE = Cchem(14,2191,34,1025.442,8.34)
 pH,CO2_PU_s,HCO3_PU_s,CO3_PU_s = Cchem(13.8,2191,33,1024.711,8.07)
 pH,CO2_PU_l,HCO3_PU_l,CO3_PU_l = Cchem(13.5,2191,33,1024.772,8.28)
 
-st([["CO2 (uM)",CO2_IE,CO2_PU_s,CO2_PU_l],\
-    ["HCO3 (uM)",HCO3_IE,HCO3_PU_s,HCO3_PU_l],\
-    ["CO3 (uM)",CO3_IE,CO3_PU_s,CO3_PU_l]],"","Output")
+st([["CO2 (uM)",CO2_PM,CO2_IE,CO2_PU_s,CO2_PU_l],\
+    ["HCO3 (uM)",HCO3_PM,HCO3_IE,HCO3_PU_s,HCO3_PU_l],\
+    ["CO3 (uM)",CO3_PM,CO3_IE,CO3_PU_s,CO3_PU_l]],"","Output")
 
 pHred = 0.4
-pH,CO2_IE_a,HCO3_IE,CO3_IE = Cchem(14,2191,34,1025.442,8.34-pHred)
-pH,CO2_PU_s_a,HCO3_PU_s,CO3_PU_s = Cchem(13.8,2191,33,1024.711,8.07-pHred)
-pH,CO2_PU_l_a,HCO3_PU_l,CO3_PU_l = Cchem(13.5,2191,33,1024.772,8.28-pHred)
+pH,CO2_PM_a,HCO3_PM_a,CO3_PM_a = Cchem(14,2191,34,1025.442,8.102-pHred)
+pH,CO2_IE_a,HCO3_IE_a,CO3_IE_a = Cchem(14,2191,34,1025.442,8.34-pHred)
+pH,CO2_PU_s_a,HCO3_PU_s_a,CO3_PU_s_a = Cchem(13.8,2191,33,1024.711,8.07-pHred)
+pH,CO2_PU_l_a,HCO3_PU_l_a,CO3_PU_l_a = Cchem(13.5,2191,33,1024.772,8.28-pHred)
 
-st([["CO2 (uM)",CO2_IE_a,CO2_PU_s_a,CO2_PU_l_a],\
-    ["HCO3 (uM)",HCO3_IE,HCO3_PU_s,HCO3_PU_l],\
-    ["CO3 (uM)",CO3_IE,CO3_PU_s,CO3_PU_l]],"","Output_low_pH")
+st([["CO2 (uM)",CO2_PM_a,CO2_IE_a,CO2_PU_s_a,CO2_PU_l_a],\
+    ["HCO3 (uM)",HCO3_PM_a,HCO3_IE_a,HCO3_PU_s_a,HCO3_PU_l_a],\
+    ["CO3 (uM)",CO3_PM_a,CO3_IE,CO3_PU_s_a,CO3_PU_l_a]],"","Output_low_pH")
 
-w=0.4
-x = ['IE','PU (Short)','PU (Long)']
-bar1 = arange(len(x))
-bar2 = [i+w for i in bar1] 
-Before = [CO2_IE,CO2_PU_s,CO2_PU_l]
-After = [CO2_IE_a,CO2_PU_s_a,CO2_PU_l_a]
-bar(bar1,Before,w,label='Before')
-bar(bar2,After,w,label='After')
-xticks(bar1+w/2.03,x)
-ylabel('[CO$_{2}$] ($\mu$M) ')
-legend(edgecolor='k')
-sf('','CO2',300)
+
+def doublebar(ylable,IE,PU_s,PU_l,IE_a,PU_s_a,PU_l_a):
+    w=0.4
+    x = ['IE','PU (S)','PU (L)']
+    bar1 = arange(len(x))
+    bar2 = [i+w for i in bar1] 
+    Before = [IE,PU_s,PU_l]
+    After = [IE_a,PU_s_a,PU_l_a]
+    bar(bar1,Before,w,label='Before')
+    bar(bar2,After,w,label='After')
+    xticks(bar1+w/2.03,x)
+    ylabel(str(ylable) + ' ($\mu$M)')
+
+rcParams.update({'font.size': 27})
+figure(1,figsize=(24,6.5))
+subplot(1,3,1)
+doublebar('[CO$_{2}$]',CO2_IE,CO2_PU_s,CO2_PU_l,CO2_IE_a,CO2_PU_s_a,CO2_PU_l_a)
+legend(loc=2,edgecolor='k')
+
+subplot(1,3,2)
+doublebar('[HCO$_{3}^{-}$]',HCO3_IE,HCO3_PU_s,HCO3_PU_l,HCO3_IE_a,HCO3_PU_s_a,HCO3_PU_l_a)
+
+subplot(1,3,3)
+doublebar('[CO$_{3}^{2-}$]',CO3_IE,CO3_PU_s,CO3_PU_l,CO3_IE_a,CO3_PU_s_a,CO3_PU_l_a)
+
+sf('','C shift',300)
 
 show()
 
